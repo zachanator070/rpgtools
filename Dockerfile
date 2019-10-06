1c1995c952b6f6535c887503d20f7a14ff6f0d80
Zach Johnson <zjohnson@adobe.com>
2019-10-06 00:13:21 -0600

    working graphql + react in docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:latest
+
+WORKDIR /home/node/app
+ADD package.json package.json
+ADD webpack.config.js webpack.config.js
+ADD webpack-dev.config.js webpack-dev.config.js
+RUN npm install
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/app/index.js'
+	},
+	mode: "production",
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'rpgtools',
+			template: __dirname + "/src/app/index.html"
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
+	},
+	resolve: {
+		alias: {
+			'parchment': path.resolve(__dirname, 'node_modules/parchment/src/parchment.ts'),
+			'quill$': path.resolve(__dirname, 'node_modules/quill/quill.js'),
+		},
+		extensions: ['.js', '.ts', '.svg']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+						plugins: ['@babel/plugin-proposal-object-rest-spread']
+					}
+				}
+			},
+			{
+				test: /\.ts$/,
+				use: [{
+					loader: 'ts-loader',
+					options: {
+						compilerOptions: {
+							declaration: false,
+							target: 'es5',
+							module: 'commonjs'
+						},
+						transpileOnly: true
+					}
+				}]
+			},
+			{
+				test: /\.svg$/,
+				use: [{
+					loader: 'html-loader',
+					options: {
+						minimize: true
+					}
+				}]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	}
+};
