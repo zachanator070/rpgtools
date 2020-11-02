fd3e953185251ef2ea43b34f7ed09f332c787dcc
Zach Johnson <zjohnson@adobe.com>
2020-11-02 00:14:14 -0700

    added bonus to dice roller

--- packages/frontend/src/components/game/DiceIcon.js
+++ packages/frontend/src/components/game/DiceIcon.js
@@ -1,26 +1,33 @@
 import React from "react";
-import { Badge, Tooltip } from "antd";
+import {Badge, InputNumber, Tooltip} from "antd";
+import {SET_BONUS_ACTION, SET_COUNT_ACTION} from "./DiceRoller";
 
-export const DiceIcon = ({ setCount, count, icon, tooltip }) => {
+export const DiceIcon = ({ reducer, count, bonus, icon, tooltip }) => {
 	return (
 		<Tooltip title={tooltip}>
-			<div
-				style={{
-					flex: "1",
-					width: "5em",
-				}}
-				onClick={() => setCount(count + 1)}
-				onContextMenu={(e) => {
-					if (count > 0) {
-						setCount(count - 1);
-					}
-					e.preventDefault();
-				}}
-			>
+			<div className={'margin-md'}>
+				<span
+					style={{
+						width: "5em",
+					}}
+					onClick={() => reducer({type: SET_COUNT_ACTION, payload: count + 1 })}
+					onContextMenu={(e) => {
+						if (count > 0) {
+							reducer({type: SET_COUNT_ACTION, payload: count - 1});
+						}
+						e.preventDefault();
+					}}
+				>
 				<Badge count={count}>
 					<img src={icon} style={{ width: "4em" }} alt={tooltip} />
 				</Badge>
+			</span>
+				<span className={'margin-md'}>
+				+
+			</span>
+				<InputNumber value={bonus} onChange={(value) => reducer({type: SET_BONUS_ACTION, payload: value})}/>
 			</div>
+
 		</Tooltip>
 	);
 };
