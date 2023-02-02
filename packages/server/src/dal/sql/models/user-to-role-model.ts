e722413c40204fc639dee90c69fbfee478c15db2
Zachary Johnson <zach.johnson070@gmail.com>
2023-02-01 22:06:56 -0700

    electron app building and working correctly (#157)
    * electron app building and working correctly
    
    * skipping load-5e-content test
    
    * try to fix flaky test
    
    ---------
    
    Co-authored-by: Zach Johnson <zjohnson@adobe.com>

--- packages/server/src/dal/sql/models/user-to-role-model.ts
+++ packages/server/src/dal/sql/models/user-to-role-model.ts
@@ -5,6 +5,11 @@ import {DataTypes} from "sequelize";
 export default class UserToRoleModel extends SqlModel {
 
     static attributes = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         UserId: DataTypes.UUID,
         RoleId: DataTypes.UUID
     };
