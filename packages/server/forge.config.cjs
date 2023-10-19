c71ef2b0d9cd5ec97bc8b3cc146a0cf6cec09cb6
Zach Johnson <zach.johnson070@gmail.com>
2023-10-18 22:16:09 -0600

    fix electron maker version

--- packages/server/forge.config.cjs
+++ packages/server/forge.config.cjs
@@ -1,39 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 
-const config = {
-  packagerConfig: { prune: false, executableName: '@rpgtools-server' },
-  rebuildConfig: {},
-  hooks: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {bin: 'rpgtools-server', name: 'rpgtools-server', artifactName: 'rpgtools-server', appName: 'rpgtools-server'},
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {bin: '@rpgtools-server'},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {bin: '@rpgtools-server'},
-    },
-    {
-      name: '@electron-forge/maker-dmg',
-      config: {bin: '@rpgtools-server'}
-    }
-  ],
-};
+let config = {};
+
 
 try {
   const fileContents = fs.readFileSync('../../package.json', 'utf8');
   const data = JSON.parse(fileContents);
-  console.log(`Building Electron App version ${data.version}`);
-  config.appVersion = data.version;
-  config.packagerConfig.appVersion = data.version;
-  for(let maker of config.makers) {
-    maker.config.version = data.version;
-  }
+  const version = data.version;
+  console.log(`Building Electron App version ${version}`);
+
+  config = {
+    packagerConfig: { prune: false, executableName: '@rpgtools-server' },
+    rebuildConfig: {},
+    hooks: {},
+    makers: [
+      {
+        name: '@electron-forge/maker-squirrel',
+        config: {bin: 'rpgtools-server', name: 'rpgtools-server', artifactName: 'rpgtools-server', appName: 'rpgtools-server', setupExe: `@rpgtools-server-${version} Setup`, version},
+      },
+      {
+        name: '@electron-forge/maker-deb',
+        config: {bin: '@rpgtools-server', version},
+      },
+      {
+        name: '@electron-forge/maker-rpm',
+        config: {bin: '@rpgtools-server', version},
+      },
+      {
+        name: '@electron-forge/maker-dmg',
+        config: {bin: '@rpgtools-server', name: `@rpgtools-server-${version}-x64`}
+      }
+    ],
+  };
+
 } catch (err) {
   console.error(err);
   throw err;
