1774e3f56ec700023523994711c1fad4cbec0e28
Zach Johnson <zach.johnson070@gmail.com>
2025-12-11 19:59:16 -0700

    fixed change location with strokes

--- packages/server/src/dal/sql/repository/sql-stroke-repository.ts
+++ packages/server/src/dal/sql/repository/sql-stroke-repository.ts
@@ -55,7 +55,14 @@ export default class SqlStrokeRepository extends AbstractSqlRepository<Stroke, S
     }
 
     async deleteAllByGameId(id: string): Promise<void> {
-        await this.staticModel.destroy({where: {GameId: id}});
+        const strokes = await this.staticModel.findAll({where: {GameId: id}});
+        for (const stroke of strokes) {
+            const pathNodes = await PathNodeModel.findAll({where: {StrokeId: stroke._id}});
+            for (const pathNode of pathNodes) {
+                await pathNode.destroy();
+            }
+            await stroke.destroy();
+        }
     }
 
 }
