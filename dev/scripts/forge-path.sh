c9cbdf5335456acb871bd652469f01052700365b
Zach Johnson <zach.johnson070@gmail.com>
2026-01-03 15:35:41 -0700

    don't use bash source

--- dev/scripts/forge-path.sh
+++ dev/scripts/forge-path.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 REPO_ROOT="$(cd -- "${SCRIPT_DIR}/../.." && pwd)"
 
 OS=$(uname -s)
